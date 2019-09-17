98dbb136b7c7f0666f3bb583732b694790b96e15
Krishna009 <krish.arjun009@gmail.com>
2019-09-17 11:42:24 +0530

    Direction map helper class

---
+++ MarsRoverPuzzle/Models/DirectionMap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace MarsRoverPuzzle
+{
+    public sealed class DirectionMap
+    {
+        private static readonly Dictionary<char, Direction> directionMap = new Dictionary<char, Direction>()
+        {
+            {'N', new North()},
+            {'S', new South()},
+            {'W', new West()},
+            {'E', new East()}
+        };
+
+        public static Direction GetDirection(char directionKey) => GetDirectionMapValue(directionKey);
+
+        private static Direction GetDirectionMapValue(char key)
+        {
+            directionMap.TryGetValue(char.ToUpper(key), out Direction commandToExecute);
+            return commandToExecute;
+        }
+    }
+}
