2bf438433bf388e7de831beb44595258f3c34325
Krishna009 <krish.arjun009@gmail.com>
2019-09-17 11:37:51 +0530

    Mars Rover Model with Basic functionality implementation

---
+++ MarsRoverPuzzle/Models/MarsRover.cs
@@ -0,0 +1,60 @@
+﻿using MarsRoverPuzzle.Commands;
+
+namespace MarsRoverPuzzle
+{
+    public interface IRover
+    {
+        void Run(string commandString);
+        void TurnRight();
+        void TurnLeft();
+        void Move();
+        Direction GetCurrentDirection();
+        Coordinate GetCurrentPosition();
+    }
+
+    public class MarsRover : IRover
+    {
+        private Plateau plateau;
+        private Coordinate currentCoordinate;
+        private Direction currentDirection;
+
+        public MarsRover(Plateau plateau, Coordinate currentCoordinate, Direction currentDirection)
+        {
+            this.plateau = plateau;
+            this.currentCoordinate = currentCoordinate;
+            this.currentDirection = currentDirection;
+        }        
+
+        public void Run(string commandString)
+        {
+            var commands = new CommandParser(commandString).ToCommands();
+            foreach (var command in commands)
+                command.Execute(this);            
+        }
+
+        public void TurnLeft() => this.currentDirection = this.currentDirection.TurnLeft();
+        public void TurnRight() => this.currentDirection = this.currentDirection.TurnRight();
+        public Coordinate GetCurrentPosition() => this.currentCoordinate;
+        public Direction GetCurrentDirection() => this.currentDirection;
+
+        public override string ToString()
+        {
+            var direction = this.GetCurrentDirection();
+            var position = this.GetCurrentPosition();
+            var builder = new System.Text.StringBuilder();
+            builder.Append(position.x)
+                    .Append(" ")
+                    .Append(position.y)
+                    .Append(" ")
+                    .Append(direction.head);
+            return builder.ToString();
+        }
+
+        public void Move()
+        {
+            var positionAfterMove = this.currentCoordinate.GetNewCoordinate(currentDirection.x, currentDirection.y);
+            if(this.plateau.HasWithinBounds(positionAfterMove))
+                this.currentCoordinate = positionAfterMove;
+        }
+    }
+}
