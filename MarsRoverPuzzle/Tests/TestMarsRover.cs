0a8decead312c7a7e440d1a7db17d5f8d2861b40
Krishna009 <krish.arjun009@gmail.com>
2019-09-17 12:28:25 +0530

    Added mars rover Unit Test Cases

--- MarsRoverPuzzle/Tests/TestMarsRover.cs
+++ MarsRoverPuzzle/Tests/TestMarsRover.cs
@@ -1,18 +1,102 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace MarsRoverPuzzle.Tests
 {
     public class TestMarsRover
     {
         [Test]
-        public void TestRun()
+        public void Test_GetCurrentLocation()
         {
-            //var rover = new MarsRover()
+            var plateau = new Plateau(5, 5);
+            var coordinate = new Coordinate(3, 3);
+            var direction = DirectionMap.GetDirection('N');
+            var rover = new MarsRover(plateau, coordinate, direction);
+
+            Assert.AreEqual("3 3 N", rover.GetCurrentLocation());
+        }
+
+        [Test]
+        public void Test_TurnLeft()
+        {
+            var plateau = new Plateau(5, 5);
+            var coordinate = new Coordinate(1, 2);
+            var direction = DirectionMap.GetDirection('N');
+            var rover = new MarsRover(plateau, coordinate, direction);
+
+            rover.TurnLeft();
+
+            Assert.AreEqual("1 2 W", rover.GetCurrentLocation());
+        }
+
+        [Test]
+        public void Test_TurnRight()
+        {
+            var plateau = new Plateau(5, 5);
+            var coordinate = new Coordinate(1, 2);
+            var direction = DirectionMap.GetDirection('N');
+            var rover = new MarsRover(plateau, coordinate, direction);
+
+            rover.TurnRight();
+
+            Assert.AreEqual("1 2 E", rover.GetCurrentLocation());
+        }
+
+        [Test]
+        public void Test_Move()
+        {
+            var plateau = new Plateau(5, 5);
+            var coordinate = new Coordinate(1, 2);
+            var direction = DirectionMap.GetDirection('N');
+            var rover = new MarsRover(plateau, coordinate, direction);
+
+            rover.Move();
+
+            Assert.AreEqual("1 3 N", rover.GetCurrentLocation());
+        }
+
+        [Test]
+        public void Test_Run()
+        {
+            var plateau = new Plateau(5, 5);
+            var coordinate = new Coordinate(1, 2);
+            var direction = DirectionMap.GetDirection('N');
+            var rover = new MarsRover(plateau, coordinate, direction);
+
+            string command = "M";
+
+            rover.Run(command);
+
+            Assert.AreEqual("1 3 N", rover.GetCurrentLocation());
+        }
+
+        [Test]
+        public void Test_Run_Multiple_Commands()
+        {
+            var plateau = new Plateau(5, 5);
+            var coordinate = new Coordinate(1, 2);
+            var direction = DirectionMap.GetDirection('N');
+            var rover = new MarsRover(plateau, coordinate, direction);
+
+            string command = "LMLMLMLMM";
+
+            rover.Run(command);
+
+            Assert.AreEqual("1 3 N", rover.GetCurrentLocation());
+        }
+
+        [Test]
+        public void Test_WontDriveOffPlateau()
+        {
+            var plateau = new Plateau(5, 5  );
+            var coordinate = new Coordinate(0, 4);
+            var direction = DirectionMap.GetDirection('N');
+            var rover = new MarsRover(plateau, coordinate, direction);
+
+            string command = "RM";
+
+            rover.Run(command);
+            
+            Assert.AreEqual("1 4 E", rover.GetCurrentLocation());
         }
     }
 }
