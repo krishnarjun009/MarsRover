1b164c9bcf3e8bc1e287230b83e212a52f856d52
Krishna009 <krish.arjun009@gmail.com>
2019-09-17 14:30:12 +0530

    implemented rover sequence deployer controller

---
+++ MarsRoverPuzzle/Models/RoversDeployer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace MarsRoverPuzzle
+{
+    public class RoversDeployer
+    {
+        private readonly Queue<IRover> rovers;
+
+        public RoversDeployer()
+        {
+            rovers = new Queue<IRover>();
+        }
+
+        public RoversDeployer AddRover(IRover rover)
+        {
+            rovers.Enqueue(rover);
+            return this;
+        }
+
+        public void Deploy()
+        {
+            var rover = GetRover();
+            if (rover != null)
+            {
+                //rover.Run();
+                Deploy();
+            }
+        }
+
+        private IRover GetRover() => IsRoversAvaiable() ? rovers.Dequeue() : null;
+        public bool IsRoversAvaiable() => rovers.Count > 0;
+    }
+}
