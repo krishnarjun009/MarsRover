6f27cc2cf885d7417f1d208d76147496425a483c
Krishna009 <krish.arjun009@gmail.com>
2019-09-17 13:15:11 +0530

    implemented unit test for Plateau class

---
+++ MarsRoverPuzzle/Tests/TestPlateau.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+
+namespace MarsRoverPuzzle.Tests
+{
+    public class TestPlateau
+    {
+        [Test]
+        public void Test_HasWithinBounds()
+        {
+            var plateau = new Plateau(5, 5);
+
+            var point = new Coordinate(0, 5);
+
+            Assert.IsTrue(plateau.HasWithinBounds(point));
+        }
+
+        [Test]
+        public void Test_Has_X_OutOfMinBound()
+        {
+            var plateau = new Plateau(5, 5);
+
+            var point = new Coordinate(-1, 5);
+
+            Assert.IsFalse(plateau.HasWithinBounds(point));
+        }
+
+        [Test]
+        public void Test_Has_Y_OutOfMinBound()
+        {
+            var plateau = new Plateau(5, 5);
+
+            var point = new Coordinate(1, -1);
+
+            Assert.IsFalse(plateau.HasWithinBounds(point));
+        }
+
+        [Test]
+        public void Test_Has_X_OutOfMaxBound()
+        {
+            var plateau = new Plateau(5, 5);
+
+            var point = new Coordinate(9, 5);
+
+            Assert.IsFalse(plateau.HasWithinBounds(point));
+        }
+
+        [Test]
+        public void Test_Has_Y_OutOfMaxBound()
+        {
+            var plateau = new Plateau(5, 5);
+
+            var point = new Coordinate(1, 7);
+
+            Assert.IsFalse(plateau.HasWithinBounds(point));
+        }
+    }
+}
